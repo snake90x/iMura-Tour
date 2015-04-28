db57d1765cf7b3a245494b7f55992c4b6a45798a
snake90x <snake90x@hotmail.it>
2015-04-28 17:08:28 +0200

    Modificato il menù laterale e inserita la GET dei file

---
+++ html2/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(main);
+
+function main() {
+	$.ajax({
+		 
+		method: 'GET',
+		url: 'database.json',
+		success: function(d) {
+			for (var i=0; i<d.length; i++) {
+                var url=d[i].url,
+                    label=d[i].label;
+                console.log(url, label)
+				$('#doclist').append("<li><a href='#' onclick='openDocs(\""+url+"\", \""+label+"\");return false;'>"+d[i].label+"</a></li>");
+			}
+            alert("caricato");
+		},
+		error: function(a,b,c) {
+			alert('Nessun documento da mostrare');
+		}
+
+	});
+	
+}
--- html2/js/mainArea.js
+++ html2/js/mainArea.js
@@ -1,52 +1,36 @@
+var id=0;//id sequenziale delle diverse tab aperte dall'utente, cresce sempre
 $(document).ready(function(){
-     
   $('#tab_help').load('a.html');
-     
-  $('#btnAdd').click(function (e) { //gestisce la creazione e l'attivazione delle nuove tab e del loro contenuto
-    	var nextTab = $('#tabs li').size()+1;
-  	
-    	// create the tab
-    	$('<li><a href="#tab'+nextTab+'" data-toggle="tab"><h6>Tab '+nextTab+'</h6></a><span>X</span></li>').appendTo('#tabs');
-    	
-    	// create the tab content
-    	$('#contentmain').append('<div class="tab-pane" id="tab'+nextTab+'"><p>tab' +nextTab+' content</p></div>');
-    	
-    	// make the new tab active
-    	$('#tabs a:last').tab('show');
-  });
-  $('#tabs').on("click","span",function () { // gestisce rimozione delle tab e il ritorno alla tab help una volta rimosse tutte le altre tab
+  $('#tabs').on("click","span",function () { 
+      // gestisce rimozione delle tab e il ritorno alla tab help una volta rimosse tutte le altre tab
         var anchor = $(this).siblings('a');
         $(anchor.attr('href')).remove();
         $(this).parent().remove();
         if( $('#tabs li').size()==0){
           $('#Help').click()
-        }
-            
+        }    
   });
-
-
-     $('#btnDoc').click(function()  {
-          var nextTab = $('#tabs li').size()+1;
-          var tabExist = false;
-          var id="tab"+nextTab;
-          $("#mainArea ul li a").each(function(i){
-               if(this.text=='DOC1'){
-                    $(this).tab('show');
-                    tabExist=true;
-               }
-          });
-          if(!tabExist){
-  	
-    	// create the tab
-    	$('<li><a href="#'+id+'" data-toggle="tab"><h6>DOC1</h6></a><span>X</span></li>').appendTo('#tabs');
-    	
-    	// create the tab content
-    	$('#contentmain').append('<div class="tab-pane" id="'+id+'"></div>');
-    	$('#'+id).load('docs/11voelske.print.html');
-    	// make the new tab active
-    	$('#tabs a:last').tab('show'); 
-          }
-     }); 
-                                  
-
 });
+function openDocs (docurl, doclabel) {
+    console.log("dentro opendocs")
+    //gestisce la creazione e l'attivazione delle nuove tab e del loro contenuto, parametro corrisponde all'url del documento
+    var url = docurl,
+        label=doclabel,
+        tabExist = false;
+
+    $("#mainArea ul li a").each(function(i){
+           if(this.text==label){
+                $(this).tab('show');
+                tabExist=true;
+           }
+    });
+    if(!tabExist){
+        // create the tab
+        $('<li><a href="#tab-'+id+'" data-toggle="tab"><h6>'+label+'</h6></a><span>X</span></li>').appendTo('#tabs');
+        // create the tab content
+        $('#contentmain').append('<div class="tab-pane" id="tab-'+id+'"><p>'+label+' content</p></div>');
+        // make the new tab active
+        $('#tabs a:last').tab('show');
+        id++;
+    }
+};
