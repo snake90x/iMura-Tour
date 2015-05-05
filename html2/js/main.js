5c811246645ed680a7426263ea6131864daf3e77
snake90x <snake90x@hotmail.it>
2015-05-05 11:06:11 +0200

    prova milos

--- html2/js/main.js
+++ html2/js/main.js
@@ -11,20 +11,10 @@ function main() {
                 console.log(url, label)
 				$('#doclist').append("<li><a href='#' onclick='openDocs(\""+url+"\", \""+label+"\");return false;'>"+d[i].label+"</a></li>");
 			}
-            BootstrapDialog.show({
-                size: BootstrapDialog.SIZE_SMALL,
-                type: BootstrapDialog.TYPE_SUCCESS,
-                title: 'Success',
-                message: 'Documenti caricati con successo',
-            });
+            openWidget('docSucc');
 		},
 		error: function(a,b,c) {
-			BootstrapDialog.show({
-                size: BootstrapDialog.SIZE_SMALL,
-                type: BootstrapDialog.TYPE_WARNING,
-                title: 'Errore',
-                message: 'Non sono stati trovati documenti da caricare',
-            });
+			openWidget('docFail');
 		}
 
 	});
--- html2/js/widgets.js
+++ html2/js/widgets.js
@@ -1,14 +1,33 @@
-function openWidget(type){
+function openWidget(type, id){
     //case switch all'interno della creazione del widget che, in base alla variabile type costruisce il widget adeguato e eventualmente ogni tipo possibile di dialog del sito. Utilizzare le istanze, una per ogni dialog e il case switch per quella adeguata
-    var dialogInstance1 = new BootstrapDialog({
-            title: 'Dialog instance 1',
-            message: 'Hi Apple!'
-    });
-    //esecuzione istanza dialogInstance1.open();
-    BootstrapDialog.show({
-        size: BootstrapDialog.SIZE_SMALL,
-        type: BootstrapDialog.TYPE_PRIMARY,
-        title: 'Widget Prototipo di '+type,
-        message: 'Mi ha chiamato '+type+'</br> Guida: <a href="http://nakupanda.github.io/bootstrap3-dialog/">Bootstrap-dialog</a>'
-    });    
+    var provaWidget = new BootstrapDialog({
+            size: BootstrapDialog.SIZE_SMALL,
+            type: BootstrapDialog.TYPE_PRIMARY,
+            title: 'Widget Prototipo di '+id,
+            message: 'Mi ha chiamato '+id+'</br> Guida: <a href="http://nakupanda.github.io/bootstrap3-dialog/">Bootstrap-dialog</a>'
+        }),
+        getDocSucc = new BootstrapDialog({
+            size: BootstrapDialog.SIZE_SMALL,
+            type: BootstrapDialog.TYPE_SUCCESS,
+            title: 'Success',
+            message: 'Documenti caricati con successo'
+        }),
+        getDocFail = new BootstrapDialog({
+            size: BootstrapDialog.SIZE_SMALL,
+            type: BootstrapDialog.TYPE_WARNING,
+            title: 'Errore',
+            message: 'Non sono stati trovati documenti da caricare'
+        });
+    //gestione casi
+    switch(type){
+        case 'docFail':
+            getDocFail.open();
+            break;
+        case 'docSucc':
+            getDocSucc.open();
+            break;
+        case 'prova':
+            provaWidget.open();
+            break;
+    }   
 }
